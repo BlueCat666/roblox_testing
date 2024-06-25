bb02bb7d33869a8a125454b8469446ea0e45cb87
BlueCat666 <giorgi.piranishvili2005@gmail.com>
2024-06-25 04:51:53 +0400

    test

--- main.js
+++ main.js
@@ -6,14 +6,20 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+let Players = [];
 
 app.get("/", (req, res) => {
     console.log("reached here");
     res.sendStatus(200);
 });
 
+app.get("/players", (req, res) => {
+  res.json(Players);
+});
+
 app.post("/post", (req, res) => {
   console.log(req.body);
+  Players = req.body
   res.sendStatus(200);
 });
 
