748098e736bf411b47901e8242caf9bd572a5a6c
BlueCat666 <giorgi.piranishvili2005@gmail.com>
2024-06-25 04:55:37 +0400

    new

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 let Players = [];
