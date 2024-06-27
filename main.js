2aead8f3e5a4dc723a92f31fca53b355e86828b3
BlueCat666 <giorgi.piranishvili2005@gmail.com>
2024-06-28 00:05:10 +0400

    s

--- main.js
+++ main.js
@@ -16,7 +16,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/players", (req, res) => {
+  console.log("got here");
   res.json(Players);
+});
+
+app.get("/get-head-thumbnail", async (req, res) => {
+  const userID = req.query.userID;
+  try {
+    const { data } = await axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${userID}&size=48x48&format=Png&isCircular=false`,);
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching thumbnail:', error);
+    res.status(500).json({ error: 'Error fetching thumbnail' });
+  }
 });
 
 app.post("/post", (req, res) => {
