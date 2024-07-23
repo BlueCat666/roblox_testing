c341772ef2fe300cccb0077a477db64269e169af
BlueCat666 <giorgi.piranishvili2005@gmail.com>
2024-07-23 17:24:14 +0400

    new

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,14 +1,17 @@
-import admin from "firebase-admin";
+import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-});
+const initializeApp = () => {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
+};
 
+export { initializeApp };
 export default admin;
